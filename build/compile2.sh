69066e97ab93fb8c80be4149c29ee6f077d89270
Andrew T. Fenley <afenley@ucsd.edu>
2014-08-25 17:11:09 -0700

    installation instructions

--- build/compile2.sh
+++ build/compile2.sh
@@ -1,5 +1,4 @@
+# Compile script for the GNU version of the stress code.
 g++ -c ../lib/*.c
-#libtool --mode=link g++ ../gmxstress.c *.o -o stress_gnu.exe libxdrfile.la
 g++ -O ../gmxstress.c *.o -o stress_gnu.exe -lxdrfile -L../build
 rm *.o
-#-L /home/afenley/Research/Gilson/Projects/ccode/libxdrfile
--- build/compile2_intel.sh
+++ build/compile2_intel.sh
@@ -1,3 +1,5 @@
+# Compile script using Intel compilers (if available). Make sure the 
+# library file libxdrfile.a was also compiled using the Intel compilers!
 icpc -c ../lib/*.c
 icpc -O2 -xAVX ../gmxstress.c *.o -o stress_intel.exe -lxdrfile -L../build
 rm *.o
